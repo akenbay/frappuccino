7a49a57213190a0821862dbae7f8514d96452b17
Daniyar Kabirov <cloudlypower@gmail.com>
2025-05-07 17:56:14 +0500

    fix: order model

--- internal/models/order.go
+++ internal/models/order.go
@@ -9,7 +9,7 @@ type Order struct {
 	ID                  int             `json:"id"`
 	CustomerID          int             `json:"customer_id"`
 	Status              string          `json:"status"`
-	PaymentMethod       *string         `json:"payment_method,omitempty"`
+	PaymentMethod       string          `json:"payment_method,omitempty"`
 	TotalPrice          float64         `json:"total_price"`
 	SpecialInstructions json.RawMessage `json:"special_instructions,omitempty"`
 	Items               []OrderItem     `json:"items"`
@@ -22,6 +22,6 @@ type OrderItem struct {
 	OrderID        int             `json:"order_id"`
 	MenuItemID     int             `json:"menu_item_id"`
 	Quantity       int             `json:"quantity"`
-	PriceAtOrder   float64         `json:"price_at_order"`
 	Customizations json.RawMessage `json:"customizations,omitempty"`
+	PriceAtOrder   float64         `json:"price_at_order"`
 }
