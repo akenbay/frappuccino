7c049bbde5c8c9f8eaa3af2b6459841fd738c3da
Aibar <aibar@MacBook-Air-Madiyar.local>
2025-05-09 12:36:03 +0500

    Fix: switched to transaction in create order

--- internal/dal/order_repository.go
+++ internal/dal/order_repository.go
@@ -52,7 +52,7 @@ func (r *orderRepository) CreateOrder(ctx context.Context, order models.Order) (
 	// 2. Insert order
 
 	var id int
-	err = r.db.QueryRowContext(ctx, `
+	err = tx.QueryRowContext(ctx, `
 		INSERT INTO orders (customer_id, payment_method, total_price, special_instructions) 
 		VALUES ($1, $2, $3, $4)
 		RETURNING id`,
@@ -65,7 +65,7 @@ func (r *orderRepository) CreateOrder(ctx context.Context, order models.Order) (
 
 	// 3. Insert order items
 	for _, item := range order.Items {
-		_, err := r.db.ExecContext(ctx, `
+		_, err := tx.ExecContext(ctx, `
 			INSERT INTO order_items (order_id, menu_item_id, quantity, price_at_order, customizations)
 			VALUES ($1, $2, $3, $4, $5)`,
 			id, item.MenuItemID, item.Quantity, item.PriceAtOrder, item.Customizations,
