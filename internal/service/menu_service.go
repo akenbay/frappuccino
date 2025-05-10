f484e9b4ea9b005f8736eeb90370a86f7969837a
Daniyar Kabirov <cloudlypower@gmail.com>
2025-05-10 20:48:53 +0500

    feat: menu service

--- internal/service/menu_service.go
+++ internal/service/menu_service.go
@@ -1,1 +1,65 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"frappuccino/internal/dal"
+	"frappuccino/internal/models"
+)
+
+type MenuService interface {
+	GetAllMenu(ctx context.Context) ([]models.MenuItems, error)
+	GetMenuItemByID(ctx context.Context, id int) (models.MenuItems, error)
+	CreateMenuItem(ctx context.Context, item models.MenuItems) (int, error)
+	UpdateMenuItem(ctx context.Context, id int, item models.MenuItems) error
+	DeleteMenuItem(ctx context.Context, id int) error
+}
+
+type menuService struct {
+	menuRepo dal.MenuRepository
+}
+
+func NewMenuService(menuRepo dal.MenuRepository) MenuService {
+	return &menuService{menuRepo: menuRepo}
+}
+
+func (s *menuService) GetAllMenu(ctx context.Context) ([]models.MenuItems, error) {
+	return s.menuRepo.GetAllMenu(ctx)
+}
+
+func (s *menuService) GetMenuItemByID(ctx context.Context, id int) (models.MenuItems, error) {
+	if id <= 0 {
+		return models.MenuItems{}, fmt.Errorf("invalid menu item id")
+	}
+	return s.menuRepo.GetMenuItemByID(ctx, id)
+}
+
+func (s *menuService) CreateMenuItem(ctx context.Context, item models.MenuItems) (int, error) {
+	if item.Name == "" {
+		return 0, fmt.Errorf("invalid menu item name")
+	}
+	if item.Price <= 0 {
+		return 0, fmt.Errorf("invalid menu item price")
+	}
+	return s.menuRepo.CreateMenuItem(ctx, item)
+}
+
+func (s *menuService) UpdateMenuItem(ctx context.Context, id int, item models.MenuItems) error {
+	if id <= 0 {
+		return fmt.Errorf("invalid menu item id")
+	}
+	if item.Name == "" {
+		return fmt.Errorf("invalid menu item name")
+	}
+	if item.Price <= 0 {
+		return fmt.Errorf("invalid menu item price")
+	}
+	return s.menuRepo.UpdateMenuItem(ctx, id, item)
+}
+
+func (s *menuService) DeleteMenuItem(ctx context.Context, id int) error {
+	if id <= 0 {
+		return fmt.Errorf("invalid menu item id")
+	}
+	return s.menuRepo.DeleteMenuItem(ctx, id)
+}
